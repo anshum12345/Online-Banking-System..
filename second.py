f663894787ff07bacd69b50ee0643f4ef26292f5
anshum12345 <105919510+anshum12345@users.noreply.github.com>
2023-05-26 18:23:31 +0530

    Add files via upload

---
+++ second.py
@@ -0,0 +1,18 @@
+class Myclass:
+    def_init_(self,name):
+    self.ivar1=name
+    print("inside init")
+
+    def_call_(self,x,y):
+    print("inside call")
+print("hello:{0}".format(self.ivar1))
+sum=x+y
+    Print("the sum is {0}".format(sum))
+    return sum
+
+
+foo= Myclass('brad')
+foo(2,9)
+
+    
+
